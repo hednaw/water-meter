d5b80ffdd2bbfb18ff666a76b60754d05e36e38a
hednaw <hednaw0@protonmail.com>
2023-04-12 23:25:08 +0200

    toggling glasses finally working

--- src/Components/Atoms/Glass/Glass.jsx
+++ src/Components/Atoms/Glass/Glass.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import { useState, useContext } from "react";
 import "./Glass.css";
 import { ReactComponent as GlassEmpty } from "../../../glass_empty.svg";
 import { ReactComponent as GlassFull } from "../../../glass_full.svg";
-const Glass = ({handler}) => {
-  const [isGlassFull, setIsGlassFull] = React.useState(true);
-  
+import { GlassContext } from "../../Molecules/GlassesContainer";
+const Glass = ({ handler }) => {
+  const [isGlassFull, setIsGlassFull] = useState(true);
+  const context = useContext(GlassContext);
+  console.log(context);
 
-  const handleClick = () => {
+  const clickHandler = () => {
     setIsGlassFull(!isGlassFull);
-    console.log("test", isGlassFull);
-    
+    if (isGlassFull) {
+      context.setWater(context.water - 200);
+    } else {
+      context.setWater(context.water + 200);
+    }
   };
 
   return (
-    <div onClick={()=>{handleClick();handler()}}>
+    <div
+      onClick={() => {
+        handler();
+        clickHandler();
+      }}
+    >
       {isGlassFull ? <GlassFull /> : <GlassEmpty />}
     </div>
   );
--- src/Components/Molecules/GlassesContainer.jsx
+++ src/Components/Molecules/GlassesContainer.jsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import Glass from "../Atoms/Glass/Glass";
 import "./GlassesContainer.css";
+
+export const GlassContext = createContext(null);
+
 export const GlassesContainer = ({ numberOfGlasses }) => {
   const glassesNum = new Array(numberOfGlasses).fill(null).map((_, i) => i);
-  const glassSize = 200
+  const glassSize = 200;
   const [water, setWater] = useState(numberOfGlasses * glassSize);
-
+  const [flag, SetFlag] = useState(true);
   const handleClick = () => {
-    setWater(water - glassSize);
-    console.log(`Water ` + water);
+    if (flag) {
+      setWater(water - glassSize);
+      console.log(`Water ` + water);
+      SetFlag(!flag);
+    } else {
+      setWater(water + glassSize);
+      SetFlag(!flag);
+    }
   };
 
   return (
-    <div className="con">
-      <h1>{water}</h1>
-      {glassesNum.map((_, i) => (
-        <Glass
-          key={i}
-          handler={handleClick}
-        ></Glass>
-      ))}
-    </div>
+    <GlassContext.Provider value={{ water, setWater }}>
+      <div className="con">
+        <h1>{water}</h1>
+        {glassesNum.map((_, i) => (
+          <Glass key={i} handler={handleClick}></Glass>
+        ))}
+      </div>
+    </GlassContext.Provider>
   );
 };
 export default GlassesContainer;
