26ec826ae3f5dc26e23c7aa6c6096725bfa1ba4b
hednaw <hednaw0@protonmail.com>
2023-04-18 22:28:28 +0200

    fixed some stuff

--- src/Components/Atoms/Glass/Glass.jsx
+++ src/Components/Atoms/Glass/Glass.jsx
@@ -16,7 +16,7 @@ const Glass = ({ k }) => {
     );
 
     if (localStorage.getItem(`glass_${k}`) == null) {
-      localStorage.setItem(`glass_` + k, isGlassFull);
+      localStorage.setItem(`glass_` + k, true);
     }
 
     context.setWater( localStorage.getItem("water"))
