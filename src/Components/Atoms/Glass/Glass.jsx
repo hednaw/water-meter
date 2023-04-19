297e90aa0a5e3d885bace78f0e4134dddf4111be
hednaw <hednaw0@protonmail.com>
2023-04-20 00:28:08 +0200

    fixed a bug related to typing

--- src/Components/Atoms/Glass/Glass.jsx
+++ src/Components/Atoms/Glass/Glass.jsx
@@ -7,9 +7,6 @@ const Glass = ({ k }) => {
   const [isGlassFull, setIsGlassFull] = useState(true);
   const context = useContext(GlassContext);
 
-
-
-
   useEffect(() => {
     setIsGlassFull(
       localStorage.getItem(`glass_${k}`) === "true" ? true : false
@@ -17,41 +14,37 @@ const Glass = ({ k }) => {
 
     if (localStorage.getItem(`glass_${k}`) == null) {
       setIsGlassFull(true);
-      localStorage.setItem(`glass_` + k, true)
+      localStorage.setItem(`glass_` + k, true);
     }
 
-    context.setWater( localStorage.getItem("water"))
-
+    context.setWater(Number(localStorage.getItem("water")));
+    console.log(typeof localStorage.getItem("water"));
     if (localStorage.getItem("water") == null) {
-      localStorage.setItem("water", context.numberOfGlasses * context.glassSize);
+      localStorage.setItem(
+        "water",
+        context.numberOfGlasses * context.glassSize
+      );
     }
-
-    
-
   }, []);
 
   const clickHandler = () => {
-    console.log(context.numberOfGlasses * context.glassSize)
+    console.log(context.numberOfGlasses * context.glassSize);
     if (context.water < 0) {
       context.setWater(0);
     } else if (context.water > context.numberOfGlasses * context.glassSize) {
       console.error("More water than max");
     }
 
-
-    
-
-
     setIsGlassFull(!isGlassFull);
     localStorage.setItem(`glass_` + k, !isGlassFull);
     if (isGlassFull) {
-      context.setWater(context.water - context.glassSize);
-      localStorage.setItem("water", context.water - context.glassSize)
+      context.setWater(Number(context.water) - context.glassSize);
+      localStorage.setItem("water", context.water - context.glassSize);
+      console.log(typeof context.water);
     } else {
       context.setWater(context.water + context.glassSize);
-      localStorage.setItem("water", context.water + context.glassSize)
+      localStorage.setItem("water", context.water + context.glassSize);
     }
-    
   };
 
   return (
